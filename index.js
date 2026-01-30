06bf12752b9fb5f5cfabf4b2455a58a719ac032b
Sahan Maduwantha <madumax2006@gmail.com>
2026-01-30 17:20:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let code = require('./pair'); 
 
+const PORT = process.env.PORT || 8000;
+
+// Increase event listeners limit
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
-});
-
+// Middleware setup - IMPORTANT: Body parser should come before routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Static files (if any)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Import routes
+const codeRouter = require('./pair'); // Make sure this file exists
+
+// Routes
+app.use('/code', codeRouter);
+
+// HTML file serving
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
+
+// Start server
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star 🌟🌟🌟🌟
-
-
-Server running on http://localhost:` + PORT)
+    Server running on port: ${PORT}
+    Local: http://localhost:${PORT}
+    `);
 });
 
 module.exports = app;
